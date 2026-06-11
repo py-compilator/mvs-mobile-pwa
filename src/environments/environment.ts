bb477a9b24fa88145dd866fcd442b3c660056216
Idris Tsafack <idris.tsafack@yahoo.com>
2026-06-12 00:53:41 +0200

    refactor: creating firebase config in environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  name: 'mvs-mobile-pwa',
+  firebaseConfig : {
+    apiKey: 'AIzaSyDnzShqkLceXD12bU_i5qf_4GzD0YZ5g2Q',
+    authDomain: 'mvs-platform-mvp.firebaseapp.com',
+    databaseURL: 'https://mvs-platform-mvp-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'mvs-platform-mvp',
+    storageBucket: 'mvs-platform-mvp.firebasestorage.app',
+    messagingSenderId: '276428323328',
+    appId: '1:276428323328:web:011136d42be1f4945936e5',
+  }
+};
