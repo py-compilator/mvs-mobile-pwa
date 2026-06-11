8673a3a6713e57b19732a3346d5da6b799a3b6f6
Idris Tsafack <idris.tsafack@yahoo.com>
2026-06-12 01:20:57 +0200

    feat: implementing MVS Neighbor Google authentication.

--- src/app/pages/login/neighbor-login/neighbor-login.page.ts
+++ src/app/pages/login/neighbor-login/neighbor-login.page.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import {
   IonButton,
   IonIcon,
@@ -11,18 +11,23 @@ import {
   logoFacebook,
   logoYahoo,
 } from 'ionicons/icons';
+import { UserAuthenticationService } from '../../../services/authentication/user-authentication-service';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-neighbor-login',
   templateUrl: './neighbor-login.page.html',
   styleUrls: ['./neighbor-login.page.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [
-    IonButton,
-    IonIcon
-  ],
+  imports: [IonButton, IonIcon],
 })
 export class NeighborLoginPage {
+
+  private readonly _ngRouter = inject(Router);
+  private _firebaseAuthService = new UserAuthenticationService();
+
   constructor() {
     addIcons({
       mailOutline,
@@ -31,5 +36,10 @@ export class NeighborLoginPage {
       logoFacebook,
       logoYahoo,
     });
+  }
+
+  async handleAuthWithGoogle() {
+    await this._firebaseAuthService.signInWithGoogle();
+    await this._ngRouter.navigateByUrl(`/home`);
   }
 }
