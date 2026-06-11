f1d010c1555c0d087e831c66fb0a2ec9582bcdff
Idris Tsafack <idris.tsafack@yahoo.com>
2026-06-12 01:02:07 +0200

    chore: providing firebase app and auth config in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,16 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideIonicAngular } from '@ionic/angular/standalone';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { getAuth, provideAuth } from '@angular/fire/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideIonicAngular({})
+    provideIonicAngular({}),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
   ],
 };
