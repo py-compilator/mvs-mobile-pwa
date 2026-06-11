877451679908ca38f4774a75994d01fad34d7c9e
Idris Tsafack <idris.tsafack@yahoo.com>
2026-06-12 01:20:14 +0200

    feat: creating home page Ng component with basic and map to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,4 +10,8 @@ export const routes: Routes = [
     path: 'login',
     loadComponent: () => import('./pages/login/login.page').then((m) => m.LoginPage),
   },
+  {
+    path: 'home',
+    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+  }
 ];
