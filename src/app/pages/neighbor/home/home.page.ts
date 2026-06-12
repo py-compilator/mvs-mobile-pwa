1c956a5cf4012ac87e21d0552097d835ed595d11
Idris Tsafack <idris.tsafack@yahoo.com>
2026-06-12 03:22:50 +0200

    feat: mvs-events page filling

--- src/app/pages/neighbor/home/home.page.ts
+++ src/app/pages/neighbor/home/home.page.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { IonIcon, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { IonIcon, IonLabel, IonTabBar, IonTabButton, IonTabs } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
-  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon],
+  imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage implements OnInit {
   constructor() {}
--- src/app/pages/neighbor/mvs-events/mvs-events.page.ts
+++ src/app/pages/neighbor/mvs-events/mvs-events.page.ts
@@ -1,14 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { IonAvatar, IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonInfiniteScroll,
+  IonInfiniteScrollContent, IonItem,
+  IonLabel, IonList, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { addIcons,  } from 'ionicons';
+import {
+  chevronDownCircle,
+  chevronForwardCircle,
+  chevronUpCircle,
+  colorPalette,
+  document,
+  globe,
+} from 'ionicons/icons';
+
+import { InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-mvs-events',
   templateUrl: './mvs-events.page.html',
   styleUrls: ['./mvs-events.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonList,
+    IonItem,
+    IonAvatar,
+    IonLabel,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    IonFabList,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MvsEventsPage implements OnInit {
-  constructor() {}
+  items: string[] = [];
+  selectedItem = signal<string | null>(null);
 
-  ngOnInit() {}
+  constructor() {
+    /**
+     * Any icons you want to use in your application
+     * can be registered in app.component.ts and then
+     * referenced by name anywhere in your application.
+     */
+    addIcons({
+      chevronDownCircle,
+      chevronForwardCircle,
+      chevronUpCircle,
+      colorPalette,
+      document,
+      globe,
+    });
+  }
+
+  ngOnInit() {
+    this.generateItems();
+  }
+
+  selectItem(item: string) {
+    this.selectedItem.set(item);
+  }
+
+  private generateItems() {
+    const count = this.items.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(`Événement MVS ${count + i}`);
+    }
+  }
+
+  onIonInfinite(event: InfiniteScrollCustomEvent) {
+    this.generateItems();
+    setTimeout(() => {
+      event.target.complete();
+    }, 500);
+  }
 }
